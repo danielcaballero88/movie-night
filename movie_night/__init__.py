1a9641d224eb99fd36fa6aee7362121964aacc99
Daniel E Caballero <danielcaballero88@gmail.com>
2021-01-15 16:54:13 +0100

    naming conventions for constraints

--- movie_night/__init__.py
+++ movie_night/__init__.py
@@ -9,8 +9,9 @@ def create_app(config_name):
     # Import configuration from config file
     app.config.from_object(config[config_name])
     # Initialize database
-    from .database import db
+    from .database import db, migrate
     db.init_app(app)
+    migrate.init_app(app, db, render_as_batch=True)
     #
     with app.app_context():
         # Register Blueprints
@@ -20,7 +21,11 @@ def create_app(config_name):
         app.register_blueprint(home_bp)
         app.register_blueprint(auth_bp)
         app.register_blueprint(movies_bp, url_prefix="/movies/")
-        # Create tables (only the first time?)
-        db.create_all()
+        if False:
+            # Commented out because it is a one time thing
+            # achieved better from cli by: flask db init
+            # (needed flask_migrate for this)
+            # Create tables (only the first time?)
+            db.create_all()
         # Done
         return app
