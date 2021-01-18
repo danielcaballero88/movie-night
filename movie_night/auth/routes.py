598c4197d6c8f62ecae54d91fc27a77d79528f94
Daniel E Caballero <danielcaballero88@gmail.com>
2021-01-18 12:26:50 +0100

    flask-wtf for auth

--- movie_night/auth/routes.py
+++ movie_night/auth/routes.py
@@ -1,15 +1,19 @@
 """ Authentication pages """
-# Flask imports
+# Flask
 from flask import current_app as app
 from flask import redirect, url_for, render_template
 from flask import request, session, flash
-# Database and models imports
+# Database
 from movie_night.database import db
+# Models
 from .models import User
-# Blueprint (self) import
-from flask import Blueprint
-#
+# Forms
+from .forms import LoginForm
+# Utils
 from .utils import login_needed, logout_needed
+# Blueprint (self)
+from flask import Blueprint
+
 
 # Blueprint Configuration
 auth_bp = Blueprint(
@@ -22,6 +26,7 @@ auth_bp = Blueprint(
 @auth_bp.route("/signup/", methods=["GET", "POST"])
 @logout_needed
 def signup():
+    """ Create an account """
     if request.method == "GET":
         return render_template("signup.html")
     if request.method == "POST":
@@ -37,27 +42,35 @@ def signup():
 @auth_bp.route("/login/", methods=["GET", "POST"])
 @logout_needed
 def login():
+    """ Login to a user account """
+    form = LoginForm()
     if request.method == "GET":
-        return render_template("login.html")
+        return render_template("login.html", form=form)
+    #
     # POST
-    # Prepare session
-    session.permanent = True
-    # Get form data
-    username = request.form["username"]
-    userpass = request.form["userpass"]
-    # Authenticate User
-    found_user = User.query.filter_by(name=username).first()
-    if not found_user:
-        flash("Not a user")
-        return redirect(url_for("auth_bp.login"))
-    if userpass != found_user.password:
-        flash("Wrong password")
-        return redirect(url_for("auth_bp.login"))
-    # else: Success
-    session["username"] = username
-    # Done
-    flash("Login Success")
-    return redirect(url_for("home_bp.home"))
+    if form.validate():
+        # Prepare session
+        session.permanent = True
+        # Get form data
+        username = form.username.data
+        password = form.password.data
+        remember = form.remember.data
+        # Authenticate User
+        found_user = User.query.filter_by(name=username).first()
+        if not found_user:
+            flash("Not a user")
+            return redirect(url_for("auth_bp.login"))
+        if password != found_user.password:
+            flash("Wrong password")
+            return redirect(url_for("auth_bp.login"))
+        # else: Success
+        session["username"] = username
+        # Done
+        flash(f"Login Success for user {username}, remember={remember}")
+        return redirect(url_for("home_bp.home"))
+    # INVALID FORM
+    flash("Invalid form")
+    return render_template('login.html', title='Sign In', form=form)
 
 @auth_bp.route("/logout/", methods=["GET", "POST"])
 def logout():
